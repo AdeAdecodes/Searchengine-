a05ba565630ab489db507a2e543fab4ea2b8df58
hardecx <31935467+hardecx@users.noreply.github.com>
2019-10-10 06:47:12 +0100

    feat prod

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -6,51 +6,51 @@ import { NbMenuItem } from '@nebular/theme';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  MENU: NbMenuItem[] = [
-    {
-      title: 'DashBoard',
-      icon: 'map',
-      link: '/',
-      home: true,
-    },
-    // {
-    //   title: 'Banking',
-    //   icon: 'briefcase-outline',
-    //   link: '/banking',
-    // },
-    // {
-    //   title: 'Expenses',
-    //  icon: 'credit-card-outline',
-    //   link: '/expenses',
-    // },
+  // MENU: NbMenuItem[] = [
+  //   {
+  //     title: 'DashBoard',
+  //     icon: 'map',
+  //     link: '/',
+  //     home: true,
+  //   },
+  //   // {
+  //   //   title: 'Banking',
+  //   //   icon: 'briefcase-outline',
+  //   //   link: '/banking',
+  //   // },
+  //   // {
+  //   //   title: 'Expenses',
+  //   //  icon: 'credit-card-outline',
+  //   //   link: '/expenses',
+  //   // },
 
-    // {
-    //   title: 'Supplier',
-    //   icon: 'car-outline',
-    //   link: '/suppliers',
-    // },
-    // {
-    //   title: 'Employee',
-    //   icon: 'checkmark-square-2-outline',
-    //   link: '/employees',
-    // },
-    // {
-    //   title: 'Reports',
-    //   icon: 'book-open-outline',
-    //   link: '/reports',
-    // },
+  //   // {
+  //   //   title: 'Supplier',
+  //   //   icon: 'car-outline',
+  //   //   link: '/suppliers',
+  //   // },
+  //   // {
+  //   //   title: 'Employee',
+  //   //   icon: 'checkmark-square-2-outline',
+  //   //   link: '/employees',
+  //   // },
+  //   // {
+  //   //   title: 'Reports',
+  //   //   icon: 'book-open-outline',
+  //   //   link: '/reports',
+  //   // },
 
-    // {
-    //   title: 'Taxes',
-    //   icon: 'file-text-outline',
-    //   link: '/taxes',
-    // },
+  //   // {
+  //   //   title: 'Taxes',
+  //   //   icon: 'file-text-outline',
+  //   //   link: '/taxes',
+  //   // },
 
-    // {
-    //   title: 'Accounting',
-    //   icon: 'car-outline',
-    //   link: '/accounting',
-    // },
+  //   // {
+  //   //   title: 'Accounting',
+  //   //   icon: 'car-outline',
+  //   //   link: '/accounting',
+  //   // },
 
 
   
@@ -59,7 +59,7 @@ export class HomeComponent implements OnInit {
   
   
    
-  ];
+  // ];
   constructor() { }
 
   ngOnInit() {
